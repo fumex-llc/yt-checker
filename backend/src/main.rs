cf3eed2c6e16d575480ce52402cf680a39ff5f81
aplafoe <75529463+aplafoe@users.noreply.github.com>
2026-09-20 23:46:56 +0700

    [fix] logs level

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,7 +8,7 @@ mod ytcheck;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn"))
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
         .target(env_logger::Target::Stdout)
         .init();
     let args = crate::args::Args::parse();
